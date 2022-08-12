fa6fe644ab2648a67f3add69e901e0ff1d9c7385
Brian Chase <brianchasemusic@agmail.com>
2022-08-11 23:49:34 -0400

    updated commit id

---
+++ psuedo.js
@@ -0,0 +1,54 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+// Prompts
+
+// poject title,..............// WHEN I enter my project title
+// description,        .......// THEN this is displayed as the title of the README
+// installation instructions,
+// usage information,
+// contribution guidelines,
+// test instructions
+
+// these values are stored 
+// and added to the 
+// generated README as the:
+
+// Title of the README
+
+// and to the sections of the README entitled:
+
+// Description,
+// Installation,
+// Usage,
+// Contributing, and
+// Tests
+
+// Prompts (contd.)
+
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README
+
+    // and a notice is added to the section of the README entitled
+    // License that explains which license the application is covered under
+
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
+
+//              Prompt               //            Section pairs           //
+// Title............................ //............................. Title // Displayed as Title of README //
+
+// description ..................... // ...................... Description //
+// installation instructions ....... // ..................... Installation //
+// usage information ............... // ............................ Usage //
+// contribution guidelines ......... // ..................... Contributing //
+// test instructions ............... // ............................ Tests //
+
+// licence ......................... // .......................... License // display badge near top of readme, and description of the chosen license
+// email ........................... // .........................Questions // 
